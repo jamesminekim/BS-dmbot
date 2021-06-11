0692915969eb0873f46d7aee26e16792ae9c54f9
jamesminekim <85725568+jamesminekim@users.noreply.github.com>
2021-06-11 16:05:47 +0900

    MINE DM BOT

--- dmsend.py
+++ dmsend.py
@@ -1,7 +1,3 @@
-
-# 봉순#6959 : MASS DM BOT SOURCE
-
-
 import discord
 import asyncio
 import datetime
@@ -24,7 +20,6 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    #메시지 관리권한이 있을시 사용가능
                     if message.author.guild_permissions.manage_messages:
                         embed = discord.Embed(color=0x1DDB16, timestamp=message.created_at)
                         embed.add_field(name="★★제목★★", value=msg, inline=True)
@@ -34,4 +29,36 @@ async def on_message(message):
                     pass
 
 
-client.run('★★봇토큰★★')
+client.run('import discord
+import asyncio
+import datetime
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("봇이 정상적으로 실행되었습니다.")
+    game = discord.Game('TEST 마인')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+#/dm {할말}로 전체DM 전송
+@client.event
+async def on_message(message):
+    if message.content.startswith('/dm'):
+        for i in message.guild.members:
+            if i.bot == True:
+                pass
+            else:
+                try:
+                    msg = message.content[4:]
+                    if message.author.id == 541498261845049344:
+                    if message.author.guild_permissions.manage_messages:
+                        embed = discord.Embed(color=0x1DDB16, timestamp=message.created_at)
+                        embed.add_field(name="★★전체 DM★★", value=msg, inline=True)
+                        embed.set_footer(text="★마인 보냄 ㅋ★")
+                        await i.send(embed=embed)
+                except:
+                    pass
+
+
+client.run('ODUyNzc3MzE0MzIzMzk4NjU3.YMLwww.IeuBSBHDzH7M-kE0j1TjcxfCeIo')
